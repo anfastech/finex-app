d3162ee810c898e4c27864fcf5dd1c154b04a908
Warrior <kpanfas6@gmail.com>
2025-07-10 20:38:49 +0530

    changing src/features/auth/ actions.ts to queries.ts

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,5 +1,5 @@
 
-import { getCurrent } from "@/features/auth/actions";
+import { getCurrent } from "@/features/auth/queries";
 import { redirect } from "next/navigation";
 
 const WorkspaceIdPage = async () => {
