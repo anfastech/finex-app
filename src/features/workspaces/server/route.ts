f39f4df76123aefc95411b73769b1ee970a9723f
Warrior <kpanfas6@gmail.com>
2025-07-19 14:29:52 +0530

    updation: the invite member link, status: work fine now the workspace shownin the url

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,3 +1,4 @@
+import z from "zod";
 import { Hono } from "hono";
 import { ID, Query } from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
@@ -10,6 +11,7 @@ import { sessionMiddleware } from "@/lib/session-middleware";
 import { DATABASE_ID, IMAGE_BUCKET_ID, MEMBERS_ID, WORKSPACE_ID } from "@/config";
 
 import { createWorkspaceSchema, updateWorkspaceSchema } from "../schemas";
+import { Workspace } from "../types";
 
 // Create a Hono app for workspace-related API endpoints
 const app = new Hono()
@@ -239,10 +241,56 @@ const app = new Hono()
         {
           inviteCode: generateInviteCode(6),
         },
-      );
+      )
 
       return c.json({ data: workspace });  
      }
+  )
+  .post(
+    "/:workspaceId/join",
+    sessionMiddleware,
+    zValidator("json", z.object({ code: z.string() })),
+    async (c) => {
+      const { workspaceId } = c.req.param();
+      const { code } = c.req.valid("json");
+
+      const databases = c.get("database");
+      const user = c.get("user");
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (member) {
+        return c.json({ error: "Already a member" }, 400);
+      }
+
+      const workspace = await databases.getDocument<Workspace>(
+        DATABASE_ID,
+        WORKSPACE_ID,
+        workspaceId
+      );
+
+      if ( workspace.inviteCode !== code ) {
+        return c.json({ error: "Invalid invite code"}, 400)
+      }
+
+      await databases.createDocument(
+        DATABASE_ID,
+        MEMBERS_ID,
+        ID.unique(),
+        {
+          workspaceId,
+          userId: user.$id,
+          role: MemberRole.MEMBER,
+        },
+
+      );
+
+      return c.json({ data: workspace });
+    }
   );
 
 export default app;
