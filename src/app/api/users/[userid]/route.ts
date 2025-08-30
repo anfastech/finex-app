ab374cc6087134c5ce5ff0479fdb21ff0a243d68
Warrior <kpanfas6@gmail.com>
2025-08-30 22:01:37 +0530

    fixing #3 vercel

--- src/app/api/users/[userid]/route.ts
+++ src/app/api/users/[userid]/route.ts
@@ -1,7 +1,6 @@
-export async function GET(
-  req: Request,
-  context: { params: Record<string, string> }
-): Promise<Response> {
-  const { userid } = context.params;
-  return Response.json({ userID: userid });
+export function GET(
+    req: Request,
+    { params }: { params: { userId: string } }
+) {
+    return Response.json({ userID: params.userId });
 }
